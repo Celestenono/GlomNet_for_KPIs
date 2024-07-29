98fc57a8118f9f092a64f204c66805cfaca88d9c
Celestenono <noemie.moreau96@gmail.com>
2024-07-29 16:05:10 +0200

    change network path

--- docker_folder/src/inference.py
+++ docker_folder/src/inference.py
@@ -442,7 +442,7 @@ if __name__ == "__main__":
     # input_dir = '/Users/nmoreau/Documents/KPIs_challenge/Validation_bis/'
     # output_dir = '/Users/nmoreau/Documents/KPIs_challenge/output_dir/'
 
-    model_path = 'best_model.pth'
+    model_path = './src/best_model.pth'
 
     df = pd.DataFrame(columns=['case name', 'wsi_dice', 'wsi_F1_50', 'wsi_AP50'])
     main(input_dir, model_path, output_dir, df)
