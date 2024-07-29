9cf52b41e88ccfca9b7d5cf0a22a6d9a6c4b6afd
Celestenono <noemie.moreau96@gmail.com>
2024-07-29 11:24:51 +0200

    inf for challenge

--- source/dataloading/image_dataset.py
+++ source/dataloading/image_dataset.py
@@ -6,29 +6,29 @@ Image.MAX_IMAGE_PIXELS = None
 import numpy as np
 
 from monai.data.dataset import Dataset
-from openslide import OpenSlide
+# from openslide import OpenSlide
 import math
 import tifffile
 import scipy.ndimage as ndi
 
 MAGNIFICATIONS = {"56Nx": 80, "DN": 80, "NEP25": 40, "normal": 80}
 
-def read_image_openslide(path_image, reduce_factor, patch_size = (10000,10000)):
-    image_openslide = OpenSlide(path_image)
-    image_dimension = image_openslide.dimensions
-    array_microscope = np.full((math.ceil(image_dimension[1]/reduce_factor), math.ceil(image_dimension[0]/reduce_factor), 4), 0, dtype='uint8')
-    for x in range(0, image_dimension[0], patch_size[0]):
-        patch_size_x = min(image_dimension[0] - x, patch_size[0])
-        for y in range(0, image_dimension[1], patch_size[1]):
-            patch_size_y = min(image_dimension[1]-y, patch_size[1])
-            patch_image_pil = image_openslide.read_region((x, y), 0, (patch_size_x, patch_size_y))
-            patch_image_pil = patch_image_pil.reduce(reduce_factor)
-            patch_image_array = np.array(patch_image_pil)
-            patch_image_size = patch_image_array.shape
-            new_x = x//reduce_factor
-            new_y = y//reduce_factor
-            array_microscope[new_y:new_y+patch_image_size[0], new_x:new_x+patch_image_size[1], :] = patch_image_array
-    return array_microscope
+# def read_image_openslide(path_image, reduce_factor, patch_size = (10000,10000)):
+#     image_openslide = OpenSlide(path_image)
+#     image_dimension = image_openslide.dimensions
+#     array_microscope = np.full((math.ceil(image_dimension[1]/reduce_factor), math.ceil(image_dimension[0]/reduce_factor), 4), 0, dtype='uint8')
+#     for x in range(0, image_dimension[0], patch_size[0]):
+#         patch_size_x = min(image_dimension[0] - x, patch_size[0])
+#         for y in range(0, image_dimension[1], patch_size[1]):
+#             patch_size_y = min(image_dimension[1]-y, patch_size[1])
+#             patch_image_pil = image_openslide.read_region((x, y), 0, (patch_size_x, patch_size_y))
+#             patch_image_pil = patch_image_pil.reduce(reduce_factor)
+#             patch_image_array = np.array(patch_image_pil)
+#             patch_image_size = patch_image_array.shape
+#             new_x = x//reduce_factor
+#             new_y = y//reduce_factor
+#             array_microscope[new_y:new_y+patch_image_size[0], new_x:new_x+patch_image_size[1], :] = patch_image_array
+#     return array_microscope
 class ImageDataset(Dataset):
     def __init__(self, list_data, transform = None):
         super().__init__(data=list_data, transform=transform)
@@ -58,6 +58,6 @@ class ImageDataset(Dataset):
         return item_trans
 
 
-if __name__ == "__main__":
-    path_image = "/Users/nmoreau/Documents/KPIs_challenge/Validation_bis/56NX/img/12-174_wsi.tiff"
-    read_image_openslide(path_image, reduce_factor=4)
+# if __name__ == "__main__":
+#     path_image = "/Users/nmoreau/Documents/KPIs_challenge/Validation_bis/56NX/img/12-174_wsi.tiff"
+#     read_image_openslide(path_image, reduce_factor=4)
